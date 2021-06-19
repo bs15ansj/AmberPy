0116c1d5a31e0b59e34488b0f233dec31f8661d3
Alex <bs15ansj@leeds.ac.uk>
2021-06-19 14:55:35 +0100

    Added some more examples

--- examples/example.py
+++ examples/example.py
@@ -12,13 +12,94 @@ import logging
 
 set_logging_level(logging.INFO)
 
-def example_1():
+def protein_experiment_example_1():
+
+    '''A simple prorein experiment using all the defaults
+    
+    This function will solvate 'CTB.pdb' in a cubic water box with
+    a minimum distance of 12 Angstroms way from the protein and add
+    sodium and chloride ions to neutralise the system. Masses
+    of hydrogen atoms will be repartitioned. Minimisation 
+    will then be performed using 2500 steps of the steepest descent
+    algorithm followed by 2500 steps of the conjugate gradient 
+    algorithm before the system us equilibrated in the NVT ensemble, 
+    heating the system up to 310 K for 125 picoseconds. Finally, 
+    a production step of 100 ns will be performed in the NPT ensemble 
+    at 310 K. The non-bonded cutoff limit for all MD steps is 9 
+    Angstroms.
+    '''
+
     p = ProteinExperiment('CTB.pdb')
+    
     p.make_system()
+    
     p.add_minimisation_step()
+    
     p.add_equilibration_step()
+
     p.add_production_step()
-    #p.run()
+
+    p.run()
+
+def protein_experiment_example_2():
+
+    '''A simple prorein experiment using non-default arguments
+
+    This function will solvate 'CTB.pdb' in a truncated octahedron
+    water box at a minimum distance of 10 Angstroms way from the 
+    protein and add 100 magnesium and 100 chloride ions to neutralise 
+    the system.  Minimisation will then be performed using 5000 steps 
+    of the steepest descent algorithm followed by 2500 steps of the 
+    conjugate gradient algorithm before the system us equilibrated in 
+    the NVT ensemble, heating the system up to 300 K for 500 picoseconds. 
+    Finally, a production step of 200 ns will be performed in the NPT 
+    ensemble at 300 K. The non-bonded cutoff limit for all MD steps is 9 
+    Angstroms.
+    '''
+
+    p = ProteinExperiment('CTB.pdb', name='protein_experiment_example_2')
+    
+    p.make_system(box_size=10.0, box_shape='oct', ions={'Mg+':100, 'Cl-':100})
+    
+    p.add_minimisation_step(steepest_descent_steps=5000)
+    
+    p.add_equilibration_step(target_temperature=300, simulation_time=500)
+
+    p.add_production_step(target_temperature=300, simulation_time=200)
+
+    p.run()
+
+def protein_experiment_example_2():
+
+    '''A simple prorein experiment using all the defaults
+    
+    This function will solvate 'CTB.pdb' in a cubic water box with
+    a minimum distance of 12 Angstroms way from the protein and add
+    sodium and chloride ions to neutralise the system. Masses
+    of hydrogen atoms will be repartitioned. Minimisation 
+    will then be performed using 2500 steps of the steepest descent
+    algorithm followed by 2500 steps of the conjugate gradient 
+    algorithm before the system us equilibrated in the NVT ensemble, 
+    heating the system up to 310 K for 125 picoseconds. Finally, 
+    a production step of 1000 ns will be performed in the NPT ensemble 
+    at 310 K. The non-bonded cutoff limit for all MD steps is 9 
+    Angstroms.
+    '''
+
+    p = ProteinExperiment('CTB.pdb')
+    
+    p.make_system()
+    
+    p.add_minimisation_step()
+    
+    p.add_equilibration_step()
+
+    # A 1000 ns is too long to be performed on arc all in one go so
+    # we will add 10 production steps of 100 ns
+    for _ in range(10):
+        p.add_production_step()
+
+    p.run()
 
 def example_2():
     c = CosolventExperiment('ALA')
@@ -36,4 +117,4 @@ def example_3():
     pc.add_production_step()
     pc.run()
 
-example_2()
+example_1()
