847c234e8ddb2852654721a7cac9a23376a7acee
Alex <bs15ansj@leeds.ac.uk>
2021-09-13 14:46:19 +0100

    Increased polling frequency for production

--- amberpy/simulation.py
+++ amberpy/simulation.py
@@ -767,6 +767,8 @@ class Simulation:
             
             step_number += 1
             step_name = md_step.__str__()
+            if step_name == 'production':
+                kwargs['pollingfrequency'] == 3600
             
             attempt_number = 0
             
